6b3a5231104317b3ca7b81d54891695a2721474a
Neloy Pramanik Supto <pramanik15-4349@diu.edu.bd>
2026-04-25 02:48:09 +0600

    `Added requirements.txt and setup.py files`

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = "sleep_project",
+    version= "0.0.0",
+    author = "Neloy",
+    author_email = "neloybbs@gmail.com",
+    packages=find_packages()
+)
